7820e5167f16e4bf7f9107a2045d8cb868149f9f
Florian Boudzoumou <florian.boudzoumou@gmail.com>
2021-07-06 18:42:36 +0200

    Updates

---
+++ precache-manifest.3c6a37a2872bf7d566e204fc74bf2448.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1fb6e788ef48b008fe90dc91e30eca1",
+    "url": "/TpGithubPages/index.html"
+  },
+  {
+    "revision": "69ba2268c18daff4f1c9",
+    "url": "/TpGithubPages/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "b3145fd51920c36eb863",
+    "url": "/TpGithubPages/static/js/2.43a713d0.chunk.js"
+  },
+  {
+    "revision": "69ba2268c18daff4f1c9",
+    "url": "/TpGithubPages/static/js/main.fb030135.chunk.js"
+  },
+  {
+    "revision": "b5aa87767453006b141c",
+    "url": "/TpGithubPages/static/js/runtime-main.f910dd75.js"
+  }
+]);
